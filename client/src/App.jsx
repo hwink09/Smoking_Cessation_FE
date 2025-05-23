9d3a211304da71a7f5c839455bb9c6f0c0f64ace
hwink09 <caotranhoangminh2003@gmail.com>
2025-05-23 12:59:51 +0700

    Login/Register/Forgot-Password template

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,16 +2,15 @@ import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Login from "~/pages/Auth/Login";
 import Register from "~/pages/Auth/Register";
+import ForgotPassword from "~/pages/Auth/ForgotPassword";
 import { Navbar } from "./components/layouts/Navbar";
 import { Footer } from "./components/layouts/Footer";
 import { Hero } from "./components/hero/hero";
 import { Services } from "./components/services/Services";
 import { WorkProcess } from "./components/workProcess/WorkProcess";
 import { Pricing } from "./components/pricing/Pricing";
-
 import { ContactForm } from "./components/contactForm/ContactForm";
 import { Testimonials } from "./components/testimonials/Testimonials";
-
 
 const Layout = ({ children }) => {
   return (
@@ -20,8 +19,9 @@ const Layout = ({ children }) => {
       <main>{children}</main>
       <Footer />
     </div>
-  )
-}
+  );
+};
+
 const router = createBrowserRouter([
   {
     path: "/",
@@ -38,16 +38,32 @@ const router = createBrowserRouter([
   },
   {
     path: "/login",
-    element: <Login />,
+    element: (
+      <Layout>
+        <Login />
+      </Layout>
+    ),
   },
   {
     path: "/register",
-    element: <Register />,
+    element: (
+      <Layout>
+        <Register />
+      </Layout>
+    ),
   },
-])
+  {
+    path: "/forgot-password",
+    element: (
+      <Layout>
+        <ForgotPassword />
+      </Layout>
+    ),
+  },
+]);
 
 function App() {
-  return <RouterProvider router={router} />
+  return <RouterProvider router={router} />;
 }
 
 export default App;
