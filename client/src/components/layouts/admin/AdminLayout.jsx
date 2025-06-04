899f597a872635a219cfd84455f0af551979bb72
ngynphong <124499195+ngynphong@users.noreply.github.com>
2025-06-04 20:15:13 +0700

    change color admin layout

--- client/src/components/layouts/admin/AdminLayout.jsx
+++ client/src/components/layouts/admin/AdminLayout.jsx
@@ -2,7 +2,7 @@ import Sidebar from "./Sidebar";
 
 export default function AdminLayout({ children, admin }) {
     return (
-        <div className="flex min-h-screen bg-gray-900">
+        <div className="flex min-h-screen bg-gradient-to-br from-slate-900 via-purple-900 to-slate-900">
             <Sidebar admin={admin} />
             <main className="flex-1 p-8">{children}</main>
         </div>
