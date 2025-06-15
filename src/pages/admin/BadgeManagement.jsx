d7fa62e8b7e11b3a976eda2c9ba3d687fb045db4
hwink09 <caotranhoangminh2003@gmail.com>
2025-06-15 21:08:26 +0700

    Remove back-end from monorepo, preserving commit history

---
+++ src/pages/admin/BadgeManagement.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Badge from "~/components/admin/badge/Badge";
+import AdminLayout from "~/components/layouts/admin/AdminLayout";
+
+const admin = {
+  name: "Admin Nguyễn",
+  avatar: "https://i.pravatar.cc/150?img=3",
+  role: "Super Admin",
+};
+function BadgeManagement() {
+  return (
+    <AdminLayout admin={admin}>
+      <h1 className="text-2xl font-bold mb-2 bg-gradient-to-r from-[#6a5af9] to-[#1ecbe1] text-transparent bg-clip-text">
+        Quản lý huy hiệu
+      </h1>
+      <Badge />
+    </AdminLayout>
+  );
+}
+
+export default BadgeManagement;
---
+++ src/pages/admin/DashboardAdmin.jsx
@@ -0,0 +1,29 @@
+import AdminLayout from "../../components/layouts/admin/AdminLayout";
+import Cards from "../../components/admin/overview/Cards";
+import Overview from "~/components/admin/overview/Overview";
+import ColourfulText from "../../components/ui/colourful-text";
+import { useSelector } from "react-redux";
+
+const stats = {
+  users: 1200,
+  revenue: "120,000,000đ",
+  smokeFreeDays: 3500,
+  moneySaved: "80,000,000đ",
+  feedbacks: 320,
+};
+
+export default function DashboardAdmin() {
+  // Get user data from auth slice
+  const { user, loading } = useSelector((state) => state.auth);
+  return (
+    <AdminLayout admin={user}>
+      <div className="flex items-center justify-start relative overflow-hidden ml-6">
+        <h1 className="text-xl md:text-3xl lg:text-4xl font-bold text-center text-white relative z-10 font-sans">
+          <ColourfulText text="Overview" /> <br />
+        </h1>
+      </div>
+      <Cards stats={stats} />
+      <Overview />
+    </AdminLayout>
+  );
+}
