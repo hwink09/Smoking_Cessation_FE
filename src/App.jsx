38f49d25099de56984cf3e54c6e0e3886c8951be
Vuong-king <vuong9408@gmail.com>
2025-06-24 08:35:30 +0700

    Create BlogPage

--- src/App.jsx
+++ src/App.jsx
@@ -22,6 +22,8 @@ import ResetPasswordPage from "./pages/auth/ResetPasswordPage";
 import VerifyPage from "./pages/auth/VerifyPage";
 import NotFoundPage from "./pages/error/404Page";
 import UnauthorizedPage from "./pages/error/UnauthorizedPage";
+import BlogPage from "./pages/generic/BlogPage";
+
 
 // Admin Pages
 import DashboardAdmin from "./pages/admin/DashboardAdmin";
@@ -35,15 +37,17 @@ import UserDashboard from "./pages/user/UserDashBoard";
 import UserProgress from "./pages/user/UserProgress";
 import UserAchievement from "./pages/user/UserAchievement";
 import UserSupport from "./pages/user/UserSupport";
+import UserProfilePage from "./pages/user/UserProfilePage";
+
 
 // PrivateRoute component
 import PrivateRoute from "./PrivateRouter";
 import CommunityPage from "./pages/generic/community/CommunityPage";
 import { useAuth } from "./hooks/useAuth";
-import UserProfilePage from "./pages/user/UserProfilePage";
 
 // Layout Wrapper for common UI elements
 const Layout = () => {
+  console.log("Layout render");
   const { currentUser } = useAuth();
 
   return (
@@ -70,6 +74,7 @@ const UserRoute = ({ element }) => (
 const ProtectedRoute = ({ element }) => <PrivateRoute>{element}</PrivateRoute>;
 
 function App() {
+  console.log("App render");
   return (
     <AuthProvider>
       <Router>
@@ -79,6 +84,7 @@ function App() {
           <Route path="/login/:token" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/fogot-password" element={<ForgotPassword />} />
+
           <Route
             path="/resset-password/:token"
             element={<ResetPasswordPage />}
@@ -93,6 +99,8 @@ function App() {
           <Route element={<Layout />}>
             <Route path="/" element={<HomePages />} />
             <Route path="/community" element={<CommunityPage />} />
+          <Route path="/blog" element={<BlogPage />} />
+
           </Route>
           {/* Admin Routes (protected) */}
           <Route
@@ -133,7 +141,10 @@ function App() {
               element={<UserRoute element={<UserSupport />} />}
               path="user/support"
             />
-            <Route path="user/profile/:id" element={<UserRoute element={<UserProfilePage />} />} />
+            <Route
+              path="user/profile/:id"
+              element={<UserRoute element={<UserProfilePage />} />}
+            />
           </Route>
           {/* Error pages */}
           <Route path="/unauthorized" element={<UnauthorizedPage />} />
